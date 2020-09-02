bd4a690522e0d8cf575a649859aebdd8960726b5
laurmertea <mertealaur@yahoo.com>
2020-09-02 04:55:26 +0200

    Add first stage script description & objectives

---
+++ scripts/battlefield.py
@@ -0,0 +1,19 @@
+# Description
+# Nowadays, this game is known all over the world. Each country may have its own version of the name, 
+# sometimes the rules are different, but the meaning of the game remains the same. 
+# Below are the main rules that may be familiar to you since childhood.
+# Tic-tac-toe is a game played by two players on a 3x3 field where the duel takes place. 
+# One of the players plays as 'X', and the other player is 'O'. 
+# 'X' plays first, then the 'O' side plays, and so on.
+# The first player that writes 3 'X' or 3 'O' in a straight line (including diagonals) wins.
+#
+# Objectives
+# Your first task in this project is to print any state of the field in the console output. 
+# Do not forget to show the moves for both players.
+#
+# Example
+# The example below shows how your output might look.
+#
+# X O X
+# O X O
+# X X O
